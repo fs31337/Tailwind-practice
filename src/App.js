6e37faf9fe5225c8fc80ac7a36133d7cf1609fd9
Franco Salina <francodamiansalina@gmail.com>
2021-09-14 23:50:26 -0300

    hamburguer

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import logo from './img/logo.png'
 function App() {
   return (
     <div className="App">
-        <div className="md:bg-blue-500 xl:bg-red-500 bg-yellow-300 mx-auto">
-        <nav className="flex justify-between lg:justify-start items-center">
-          <div className="logo p-2">
-            <img src={logo} width="100%" alt="Imagen"></img>
+        <div className="mx-auto">
+        <nav className="bg-gray-800 flex justify-between lg:justify-start items-center">
+          <div className="logo p-2 w-1/6">
+            <img src={logo} width="100" alt="Imagen"></img>
           </div>
-          <div className="links">
-            <ul>
-              <li><a href="#"></a>Home</li>
-              <li><a href="#"></a>Play Together</li>
-              <li><a href="#"></a>Explore</li>
-              <li><a href="#"></a>Browser Fury</li>
-              <li><a href="#"></a>Buy Now</li>
+          <div className="links lg:block hidden w-1/6 md:w-4/6">
+            <ul className="menu flex items-center justify-center gap-5">
+              <li className="link"><a href="#"></a>Home</li>
+              <li className="link"><a href="#"></a>Play Together</li>
+              <li className="link"><a href="#"></a>Explore</li>
+              <li className="cursor-pointer border-4 border-yellow-400 text-white font-bold p-2 rounded-full hover:bg-white hover:text-black transition duration-500"><a href="#"></a>Browser Fury</li>
+              <li className="cursor-pointer rounded-full bg-red-500 text-white font-bold px-4 py-3 hover:text-black hover:bg-white transition duration-500"><a href="#"></a>Buy Now</li>
+            </ul>
+          </div>
+
+          <div className="block lg:hidden w-1/6 lg:w-4/6">
+            <a href="#" className="link" id="mobile-menu" >Menu</a>
+            <ul className="mobile-links hidden w-full absolute z-50 left-0 text-center bg-gray-800">
+              <li className="link"><a href="#"></a>Home</li>
+              <li className="link"><a href="#"></a>Play Together</li>
+              <li className="link"><a href="#"></a>Explore</li>
+              <li className="cursor-pointer border-4 border-yellow-400 text-white font-bold p-2 rounded-full hover:bg-white hover:text-black transition duration-500"><a href="#"></a>Browser Fury</li>
+              <li className="cursor-pointer rounded-full bg-red-500 text-white font-bold px-4 py-3 hover:text-black hover:bg-white transition duration-500"><a href="#"></a>Buy Now</li>
             </ul>
           </div>
         </nav>
