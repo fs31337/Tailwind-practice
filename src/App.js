2a573b15e4857e81d8297396c514bfe69c38e92c
Franco Salina <francodamiansalina@gmail.com>
2021-09-15 19:26:17 -0300

    background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import {useState} from 'react';
 import './App.css';
-import logo from './img/logo.png'
+import logo from './img/logo.png';
+import heroimg from './img/hero.png';
 
 function App() {
+  const [menu, setMenu] = useState(false);
+  const toggleClass = () => {
+    setMenu(!menu);
+  };
   return (
     <div className="App">
         <div className="mx-auto">
@@ -14,22 +20,29 @@ function App() {
               <li className="link"><a href="#"></a>Home</li>
               <li className="link"><a href="#"></a>Play Together</li>
               <li className="link"><a href="#"></a>Explore</li>
-              <li className="cursor-pointer border-4 border-yellow-400 text-white font-bold p-2 rounded-full hover:bg-white hover:text-black transition duration-500"><a href="#"></a>Browser Fury</li>
-              <li className="cursor-pointer rounded-full bg-red-500 text-white font-bold px-4 py-3 hover:text-black hover:bg-white transition duration-500"><a href="#"></a>Buy Now</li>
+              <li className="cursor-pointer border-4 border-yellow-400 text-white font-bold p-2 rounded-full hover:bg-white hover:text-black transition duration-500"><a href="#">Browser Fury</a></li>
+              <li className="cursor-pointer rounded-full bg-red-500 text-white font-bold px-4 py-3 hover:text-black hover:bg-white transition duration-500"><a href="#">Buy Now</a></li>
             </ul>
           </div>
 
           <div className="block lg:hidden w-1/6 lg:w-4/6">
-            <a href="#" className="link" id="mobile-menu" >Menu</a>
-            <ul className="mobile-links hidden w-full absolute z-50 left-0 text-center bg-gray-800">
+            <a href="#" className="link" onClick={() => toggleClass()}>Menu</a>
+            <ul className={!menu ? "hidden w-full absolute z-50 left-0 text-center bg-gray-800" : "w-full absolute z-50 left-0 text-center bg-gray-800"}>
               <li className="link"><a href="#"></a>Home</li>
               <li className="link"><a href="#"></a>Play Together</li>
               <li className="link"><a href="#"></a>Explore</li>
-              <li className="cursor-pointer border-4 border-yellow-400 text-white font-bold p-2 rounded-full hover:bg-white hover:text-black transition duration-500"><a href="#"></a>Browser Fury</li>
-              <li className="cursor-pointer rounded-full bg-red-500 text-white font-bold px-4 py-3 hover:text-black hover:bg-white transition duration-500"><a href="#"></a>Buy Now</li>
+              <li className="cursor-pointer border-4 my-4 inline-block border-yellow-400 text-white font-bold p-2 rounded-full hover:bg-white hover:text-black transition duration-500"><a href="#">Browser Fury</a></li>
+              <li className="cursor-pointer rounded-full my-4 inline-block bg-red-500 text-white font-bold px-4 py-3 hover:text-black hover:bg-white transition duration-500"><a href="#">Buy Now</a></li>
             </ul>
           </div>
         </nav>
+        <section className="header">
+          <img src={heroimg} alt="heroimg" />
+        </section>
+
+        <section>
+          <div className="bg-wave.pattern"></div>
+        </section>
       </div>
     </div>
   );
