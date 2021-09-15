72f5dcf347d1e6481ae052998fe6a9ba51b7033f
Franco Salina <francodamiansalina@gmail.com>
2021-09-14 23:27:10 -0300

    colours & breakpoints

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/logo.png'
 
 function App() {
   return (
     <div className="App">
-      <div className="text-blue-500"> Hola como estamos?</div>
+        <div className="md:bg-blue-500 xl:bg-red-500 bg-yellow-300 mx-auto">
+        <nav className="flex justify-between lg:justify-start items-center">
+          <div className="logo p-2">
+            <img src={logo} width="100%" alt="Imagen"></img>
+          </div>
+          <div className="links">
+            <ul>
+              <li><a href="#"></a>Home</li>
+              <li><a href="#"></a>Play Together</li>
+              <li><a href="#"></a>Explore</li>
+              <li><a href="#"></a>Browser Fury</li>
+              <li><a href="#"></a>Buy Now</li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
